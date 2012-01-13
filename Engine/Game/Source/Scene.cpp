70d3c5f2acfdeb17997414ff3f1e72704076573e
Douglas Eugene Reisinger II <dougr@vs-networks.com>
2012-01-13 14:50:40 -0500

    fixed default decal cube map file link

--- Engine/Game/Source/Scene.cpp
+++ Engine/Game/Source/Scene.cpp
@@ -11,7 +11,7 @@ namespace EG{
             textures->AddTexture("default_decal", new EG::Graphics::Texture("Assets/Textures/default_decal.png"));
             textures->AddTexture("default_normal", new EG::Graphics::Texture("Assets/Textures/default_normal.png"));
             textures->AddTexture("default_height", new EG::Graphics::Texture("Assets/Textures/default_height.png"));
-            textures->AddCubeMap("default_decal_cube_map", new EG::Graphics::CubeMap("Assets/default_decal/default_decal.png", "Assets/Textures/default_decal.png", "Assets/Textures/default_decal.png", "Assets/Textures/default_normal.png", "Assets/Textures/default_decal.png", "Assets/Textures/default_decal.png"));
+            textures->AddCubeMap("default_decal_cube_map", new EG::Graphics::CubeMap("Assets/Textures/default_decal.png", "Assets/Textures/default_decal.png", "Assets/Textures/default_decal.png", "Assets/Textures/default_normal.png", "Assets/Textures/default_decal.png", "Assets/Textures/default_decal.png"));
             textures->AddCubeMap("default_normal_cube_map", new EG::Graphics::CubeMap("Assets/Textures/default_normal.png", "Assets/Textures/default_normal.png", "Assets/Textures/default_normal.png", "Assets/Textures/default_normal.png", "Assets/Textures/default_normal.png", "Assets/Textures/default_normal.png"));
             textures->AddCubeMap("default_height_cube_map", new EG::Graphics::CubeMap("Assets/Textures/default_height.png", "Assets/Textures/default_height.png", "Assets/Textures/default_height.png", "Assets/Textures/default_height.png", "Assets/Textures/default_height.png", "Assets/Textures/default_height.png"));
         }
