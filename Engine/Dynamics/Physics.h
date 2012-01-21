b9bb55b475320bf6cd3f53b24bb813fa3159fb01
Douglas Eugene Reisinger II <palodequeso@gmail.com>
2012-01-21 01:45:41 -0500

    started working on physics/dynamics again

--- Engine/Dynamics/Physics.h
+++ Engine/Dynamics/Physics.h
@@ -1,1 +1,124 @@
- 
+#ifndef EG_DYNAMICS_PHYSICS_H
+#define EG_DYNAMICS_PHYSICS_H
+
+#include <bullet/btBulletDynamicsCommon.h>
+
+namespace EG{
+	namespace Dynamics{
+		class CollisionShape{
+			public:
+				CollisionShape(void);
+				~CollisionShape(void);
+
+				enum CollisionShapeType{
+					SPHERE = 0,
+					BOX = 1,
+					CONE = 2,
+					CYLINDER = 3,
+					PLANE = 4,
+					POLYSHAPE = 5,
+					COMPOUND = 6,
+					HEIGHTS = 7
+				};
+
+				CollisionShapeType GetShapeType(void);
+
+				void SetMass(float _mass);
+				float GetMass(void);
+			protected:
+				CollisionShapeType shape_type;
+				float mass;
+		};
+
+		class CollisionSphere : public CollisionShape{
+			public:
+				CollisionSphere(void);
+				~CollisionSphere(void);
+			private:
+				//
+		};
+
+		class CollisionBox : public CollisionShape{
+			public:
+				CollisionBox(void);
+				~CollisionBox(void);
+			private:
+				//
+		};
+
+		class CollisionCone : public CollisionShape{
+			public:
+				CollisionCone(void);
+				~CollisionCone(void);
+			private:
+				//
+		};
+
+		class CollisionCylinder : public CollisionShape{
+			public:
+				CollisionCylinder(void);
+				~CollisionCylinder(void);
+			private:
+				//
+		};
+
+		class CollisionPlane : public CollisionShape{
+			public:
+				CollisionPlane(void);
+				~CollisionPlane(void);
+			private:
+				//
+		};
+
+		class CollisionPolys : public CollisionShape{
+			public:
+				CollisionPolys(void);
+				~CollisionPolys(void);
+			private:
+				//
+		};
+
+		class CollisionHeights : public CollisionShape{
+			public:
+				CollisionHeights(void);
+				~CollisionHeights(void);
+			private:
+				//
+		};
+
+		class CollisionCompoundShape : public CollisionShape{
+			public:
+				CollisionCompoundShape(void);
+				~CollisionCompoundShape(void);
+			private:
+				std::vector<CollisionShape *> shapes;
+		};
+
+		class RigidBody{
+			public:
+				RigidBody(void);
+				~RigidBody(void);
+			private:
+				btRigidBody *bt_rigid_body;
+				CollisionShape *shape;
+		};
+
+		class Physics{
+			public:
+				Physics(void);
+				~Physics(void);
+
+				void Update(float frame_time);
+
+				void AddRigidBody(RigidBody *body);
+			private:
+				btBroadphaseInterface *broadphase;
+				btDefaultCollisionConfiguration *configuration;
+				btCollisionDispatcher *dispatcher;
+				btSequentialImpulseConstraintSolver *solver;
+				btDiscreteDynamicsWorld *world;
+		};
+	}
+}
+
+#endif
--- Engine/Dynamics/Source/Physics.cpp
+++ Engine/Dynamics/Source/Physics.cpp
@@ -1,1 +1,26 @@
- 
+#include "../Physics.h"
+
+namespace EG{
+	namespace Dynamics{
+		Physics::Physics(void){
+			broadphase = new btDbvtBroadphase();
+			configuration = new btDefaultCollisionConfiguration();
+			dispatcher = new btCollisionDispatcher(configuration);
+			solver = new btSequentialImpulseConstraintSolver();
+			world = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, configuration);
+			world->setGravity(btVector3(0, -10, 0));
+		}
+
+		Physics::~Physics(void){
+			delete world;
+			delete solver;
+			delete dispatcher;
+			delete configuration;
+			delete broadphase;
+		}
+
+		void Physics::Update(float frame_time){
+			world->stepSimulation(frame_time, 10);
+		}
+	}
+}
