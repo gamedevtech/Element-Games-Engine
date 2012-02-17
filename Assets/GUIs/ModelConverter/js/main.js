a13928919687b4cc75687448e95c7fd21ee3520a
Douglas Eugene Reisinger II <dougr@vs-networks.com>
2012-02-17 17:39:13 -0500

    head is setup with underscore and such

--- Assets/GUIs/ModelConverter/js/main.js
+++ Assets/GUIs/ModelConverter/js/main.js
@@ -1,4 +1,5 @@
-head.js("js/external/underscore-min.js", "/js/external/backbone-min.js", "/js/external/jquery-1.7.1.min.js", "/js/external/jquery-ui-1.8.17.custom.min.js", function() {
+head.js("js/external/jquery-1.7.1.min.js", "js/external/jquery-ui-1.8.17.custom.min.js", "js/external/underscore-min.js", "js/external/backbone-min.js",
+function() {
 	alert("Howdy!");
-}
+});
 
