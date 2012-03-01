9791d84c449d0bd7a46b8ff296e02e77edbabcb5
Douglas Eugene Reisinger II <palodequeso@gmail.com>
2012-02-29 21:35:47 -0500

    starting writing basic dispatching routines

--- Server/modules/data_retreival.py
+++ Server/modules/data_retreival.py
@@ -1,41 +1,49 @@
 from generator import Generator
+from dispatch_handlers.communication import Communication
+from dispatch_handlers.movement import Movement
 
 class DataRetreival:
     def __init__(self, db):
         self.db = db
         self.generator = Generator(self.db)
         self.request_index = {
-            '0000': 'ping',
-            '0001': 'send_message',
-            '0002': 'login',
-            '0003': 'get_surroundings'
+            0: 'ping',
+            1: 'message',
+            2: 'login'
         }
 
-    def HandleRequest(self, request_data):
-        request_type = self.request_index.get(request_data[0:4], None)
-        message = request_data[4:]
-        if request_type:
-            if request_type == 'ping':
-                return 'pong'
-            elif request_type == 'send_message':
-                return 'Mesage Received: %s' % message
-            elif request_type == 'login':
-                creds = message.split(':')
-                username = creds[0]
-                password = creds[1]
-                print username
-                print password
-                row = self.db.FindOne('users', {'username': username, 'password': password})
-                print row
-                if row:
-                    return "success"
-                return "failure"
-            elif request_type == 'get_surroundings':
-                position = message.split(':')
-                position[0] = float(position[0])
-                position[1] = float(position[1])
-                position[2] = float(position[2])
-                sector = self.generator.GetSector(position)
-                return "Got Sector: %f, %f, %f" % (position[0], position[1], position[2])
-        else:
+    def ActionDispatcher(self, integer_index, message):
+        if integer_index < 10:
             return None
+
+        mod = None
+        if integer_index >= 10 and integer_index < 30:
+            mod = Communication(self.db)
+        if integer_index >= 30 and integer_index < 50:
+            mod = Movement(self.db)
+
+        return mod.Handle(integer_index, message)
+
+    def HandleRequest(self, request_data):
+        request_int = int(request_data[0:4])
+        message = request_data[4:]
+
+        dispatcher_result = self.ActionDispatcher(request_int, message)
+
+        if not dispatcher_result:
+            request_type = self.request_index.get(request_int, None)
+            if request_type:
+                if request_type == 'ping':
+                    return 'pong'
+                elif request_type == 'message':
+                    return 'Mesage Received: %s' % message
+                elif request_type == 'login':
+                    creds = message.split(':')
+                    username = creds[0]
+                    password = creds[1]
+                    row = self.db.FindOne('users', {'username': username, 'password': password})
+                    if row:
+                        return "success"
+                    return "failure"
+            return None
+        return dispatcher_result
